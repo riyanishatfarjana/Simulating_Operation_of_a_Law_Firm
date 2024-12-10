9a9b11788f6a7d785b113c0c7173e931179cf81c
farjana <nishatriya56@gmail.com>
2024-12-11 01:52:10 +0600

    x

---
+++ src/main/resources/com/example/group16/Farjana_2220804/FirmManager1/Assigntask.java
@@ -0,0 +1,22 @@
+public class Assigntask
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void Task2FXID(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void Task1FXID(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void SaveOnMouseclick(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void BackOnMouseClick(ActionEvent actionEvent) {
+    }
+}
