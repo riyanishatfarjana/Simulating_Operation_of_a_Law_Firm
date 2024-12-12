3fec65255b583cb5e528c62b47651aa53cff3b5a
farjana <nishatriya56@gmail.com>
2024-12-13 02:53:46 +0600

    x

--- src/main/java/com/example/group16/Farjana_2220804/Lawyer/Dashboard.java
+++
@@ -1,4 +0,0 @@
-package com.example.group16.Farjana_2220804.Lawyer;
-
-public class Dashboard {
-}
---
+++ src/main/java/com/example/group16/Farjana_2220804/Lawyer/DashboardController.java
@@ -0,0 +1,7 @@
+package com.example.group16.Farjana_2220804.Lawyer;
+
+public class DashboardController
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
